db4418b17db0afae519e3a2d3c578de9ed668b26
Quinn1876 <qhodges0442@gmail.com>
2018-10-27 02:53:22 -0400

    Added By-line

--- oopMain.py
+++ oopMain.py
@@ -1,3 +1,5 @@
+# Created By Quinn Hodges
+
 import requests
 import json
 import pprint
@@ -15,29 +17,32 @@ class IMG_SCRUBBER():
 		'formatversion' : 2,
 	}
 	
-	index = 0
+	img_index = 0
 	cache = []
 	img_url = ''
-	base_sight = 'https://commons.wikimedia.org/wiki/'
+	base_site = 'https://commons.wikimedia.org/wiki/'
 	
 	@classmethod
 	def setTitle(cls):
-		ui = input('What image would you like?')
-		cls.query{'titles'} = ui
+		ui = input('What image would you like? ')
+		cls.query['titles'] = ui
 		return
 	
 	@classmethod
 	def setNumber(cls):
 		ui = ''
 		while type(ui) != type(0):
-			ui = input("Enter a number between 1 and 10")
-			if type(ui) != type(0):
+			ui = input("Enter a number between 1 and 10: ")
+			try:
+				ui = int(ui)
+				if int(ui) - 1  in range (10):
+					cls.img_index = ui
+				else:
+					ui = ''
+					print('Error: Not in Range')
+			except ValueError:
 				print("Error: Not a number")
-			else if int(ui) - 1  in range (10):
-				cls.img_index = ui
-			else:
-				ui = ''
-				print('Error: Not in Range')
+			
 		return
 	
 	@classmethod	
@@ -48,34 +53,37 @@ class IMG_SCRUBBER():
 			if i['title'].endswith(('.jpg', '.JPG', '.jpeg')):
 				cls.cache.append(i['title']) # Add every jpeg file name to a list of images
 		
-		r = requests.get(base_site + cls.cache[cls.index]) # Query the General Wiki page of a given image specified by the index of imageCache
-		bCache = r.content # Caches the binary from the GET request
-		bCache = str(bCache).lstrip("b'")
+		try:
+			r = requests.get(cls.base_site + cls.cache[cls.img_index]) # Query the General Wiki page of a given image specified by the index of imageCache
+			bCache = r.content # Caches the binary from the GET request
+			bCache = str(bCache).lstrip("b'")
 		
-		parser = MyParser()
-		parser.feed(bCache)
-		im = requests.get(cla.url) # Querys the url of the Image
-		Image.open(BytesIO(im.content)).show() # Converts the binary to an image and opens it
+			parser = MyParser()
+			parser.feed(bCache)
+			im = requests.get(cls.img_url) # Querys the url of the Image
+			Image.open(BytesIO(im.content)).show() # Converts the binary to an image and opens it
+		except IndexError:
+			print("Image not found")
 		
-class MyParser(HMTMLParser):
+		
+class MyParser(HTMLParser):
 	# Searches for an image tag that has the same name as the desired Image
 	def handle_starttag(self, tag, attrs):
 		if tag == 'img':
-            print(tag)
-            for i in attrs:
-                if i[0] == 'alt' and i[1] == IMG_SCRUBBER.cache[IMG_SCRUBBER.index]:
-                    for a in attrs:
-                        if a[0] == 'src':
-                            IMG_SCRUBBER.img_url = a[1]
+			for i in attrs:
+				if i[0] == 'alt' and i[1] == IMG_SCRUBBER.cache[IMG_SCRUBBER.img_index]:
+					for a in attrs:
+						if a[0] == 'src':
+							IMG_SCRUBBER.img_url = a[1]
                             
                             
             
 
-    def handle_endtag(self, tag):
-        return super().handle_endtag(tag)
+	def handle_endtag(self, tag):
+		return super().handle_endtag(tag)
 
-    def handle_data(self, tag):
-         return super().handle_data(tag)
+	def handle_data(self, tag):
+		return super().handle_data(tag)
 		
 	
 
@@ -83,5 +91,6 @@ class MyParser(HMTMLParser):
 if __name__ == '__main__':
 	IMG_SCRUBBER.setTitle()
 	IMG_SCRUBBER.setNumber()
+	IMG_SCRUBBER.runQuery()
 	
 	
