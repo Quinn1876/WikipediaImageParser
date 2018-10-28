a0225ccab3d1b2e17a8985075a8d540384bd2585
Quinn1876 <qhodges0442@gmail.com>
2018-10-28 03:04:07 -0400

    Version 1 of User Interface is complete

--- oopMain.py
+++ oopMain.py
@@ -23,7 +23,7 @@ class IMG_SCRUBBER():
 	base_site = 'https://commons.wikimedia.org/wiki/'
 	
 	@classmethod
-	def setTitle(cls):
+	def setTitle(cls, i):
 		ui = input('What image would you like? ')
 		cls.query['titles'] = ui
 		return
@@ -45,14 +45,35 @@ class IMG_SCRUBBER():
 			
 		return
 	
+	# Direct name input for Access from a non-consol UI
+	@classmethod
+	def setImageName(cls, name): 
+		cls.query['titles'] = name
+	
+	@classmethod
+	def setImageNumber(cls, number):
+		cls.img_index = number
+	
 	@classmethod	
-	def runQuery(cls):
+	def runFullQuery(cls):
+		getImageList()
+		openImage(getImage())
+		
+			
+	@classmethod
+	def getImageList(cls):
+		del cls.cache
+		cls.cache = []
 		r = requests.get('https://en.wikipedia.org/w/api.php', params=cls.query) # Query the main wiki API for a list of images
 		dict = json.loads(r.content) # Convert the responce to easily parsable Json
 		for i in dict['query']['pages'][0]['images']:
 			if i['title'].endswith(('.jpg', '.JPG', '.jpeg')):
-				cls.cache.append(i['title']) # Add every jpeg file name to a list of images
-		
+				cls.cache.append(str(i['title'])) # Add every jpeg file name to a list of images
+				
+		return cls.cache
+	
+	@classmethod
+	def getImage(cls):
 		try:
 			r = requests.get(cls.base_site + cls.cache[cls.img_index]) # Query the General Wiki page of a given image specified by the index of imageCache
 			bCache = r.content # Caches the binary from the GET request
@@ -61,9 +82,13 @@ class IMG_SCRUBBER():
 			parser = MyParser()
 			parser.feed(bCache)
 			im = requests.get(cls.img_url) # Querys the url of the Image
-			Image.open(BytesIO(im.content)).show() # Converts the binary to an image and opens it
+			return Image.open(BytesIO(im.content))
 		except IndexError:
 			print("Image not found")
+			
+	@classmethod
+	def openImage(cls, img):
+		img.show() # Converts the binary to an image and opens it
 		
 		
 class MyParser(HTMLParser):
@@ -91,6 +116,6 @@ class MyParser(HTMLParser):
 if __name__ == '__main__':
 	IMG_SCRUBBER.setTitle()
 	IMG_SCRUBBER.setNumber()
-	IMG_SCRUBBER.runQuery()
+	IMG_SCRUBBER.runFullQuery()
 	
 	
